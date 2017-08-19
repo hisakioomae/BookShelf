0cdd1c8a3326b232017630577de9c6c48402abc7
Hisaki Ohmae <hisaki0616@gmail.com>
2017-08-19 11:32:39 +0000

    added category

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819100001) do
+ActiveRecord::Schema.define(version: 20170819112012) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books_categories", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "book_id",     null: false
+  end
+
+  add_index "books_categories", ["book_id"], name: "index_books_categories_on_book_id"
+  add_index "books_categories", ["category_id"], name: "index_books_categories_on_category_id"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
